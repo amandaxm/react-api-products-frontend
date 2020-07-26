c815fedd58b87a189e6cbef02fbaff53c4061f97
Amanda Xavier Mariano <42248042+amandaxm@users.noreply.github.com>
2020-07-26 19:29:58 -0300

    Paginação da listagem

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,35 +1,66 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import api from '../../service/api';
 import './styles.css';
-export default class Main extends Component{
-    state ={
-        products:[],
-    }
-    componentDidMount(){//assim que o componente for mostrado em tela
+export default class Main extends Component {
+    state = {
+        products: [],
+        productInfo: {},
+        page: 1,
+    };
+    componentDidMount() {//assim que o componente for mostrado em tela
         this.loadProducts();
 
     }
-    loadProducts= async()=>{
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         //console.log(response.data.docs);
-        this.setState({products: response.data.docs});
+        const { docs, ...productInfo } = response.data;
+        this.setState({ products: docs, productInfo, page });
     };
-    render(){
-        const {products} = this.state;
-    return (
-        <div className="product-list">
-            {products.map(product=>(
-                <article key={product._id}> 
-                    <strong>{product.title} </strong>
-                    <p>{product.description} </p>
-                    <a href="">{product.url}</a>
-                 </article>
-            )
-    
-            )}
 
-        </div>
-    )
+    nextPage = () => {
+        const { page, productInfo } = this.state;//nuscar a pagina atual e product info
+        //verificar se a pagina atual já e a ultima pagina
+        if (page == productInfo.pages) {
+            return;
+
+        } else {
+            const pageNumber = page + 1;//pegar proxima pagina
+            this.loadProducts(pageNumber);
+
+        }
+    };
+
+    prevPage = () => {
+        const { page } = this.state;//nuscar a pagina atual e product info
+        //verificar se a pagina atual já e a ultima pagina
+        if (page == 1) {
+            return;
+        } else {
+
+            const pageNumber = page - 1;//pegar proxima pagina
+
+            this.loadProducts(pageNumber);
+        }
+    };
+    render() {
+        const { products, page, productInfo } = this.state;
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title} </strong>
+                        <p>{product.description} </p>
+                        <a href="">Acessar</a>
+                    </article>
+                )
+                )}
+                <div className="actions">
+                    <button disable={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disable={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+                </div>
+            </div>
+        )
     }
 
 
